f5770c6ca2ec513da73fd0d24f76c9e49899307c
chandlerblaschka <cblaschka@live.com>
2022-01-24 15:40:49 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import TopBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <h1>Hello World</h1>
     </div>
   );
