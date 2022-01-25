af670d4903f86349654454a0b3214d47713162ff
chandlerblaschka <cblaschka@live.com>
2022-01-25 14:40:21 -0600

    hero and nav bar finished

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,38 @@
 import Card from 'react-bootstrap/Card'
 import image from '../images/hero_image.png'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Flag from '../images/flag.png'
+import Form from 'react-bootstrap/Form'
+import { FormGroup } from 'react-bootstrap/FormGroup'
+
 function Hero() {
     return (
         <div>
-            <Card className="" style={{ "display": "flex", "background": "DEEDD6" }}>
-                <Card.Img src={image} alt="Card image"
-                    style={{ "height": "500px", "width": "fit-content", "display": "flex", "alignItems": "flex-end" }}
+            <Card className="heroCard">
+                <Card.Img src={image} alt="Card image" className='heroImage'
+                    style={{ "height": "400px", "width": "fit-content" }}
                 />
-                <Card.ImgOverlay>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
+                <Card.ImgOverlay className='heroText'>
+                    <Dropdown as={ButtonGroup} className='countryDrop'>
+                        <Button variant="info" className='dropButton'><img src={Flag} style={{ "height": "15px", "width": "15px", "marginBottom": "5px" }} /></Button>
+                        <Dropdown.Toggle split variant="info" id="dropdown-split-basic" className='dropArrow' />
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">US</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">CA</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Card.Title className='heroTitle'>Order groceries for delivery or pickup today</Card.Title>
+                    <Card.Text className='heroFinePrint'>
+                        Whatever you want from local stores, brought right to your door.
                     </Card.Text>
-                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formGridAddress1" style={{ width: "20%", marginLeft: "7%" }}>
+                            <Form.Label></Form.Label>
+                            <Form.Control placeholder="Enter your address" />
+                        </Form.Group>
+                    </Form>
                 </Card.ImgOverlay>
             </Card>
         </div>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -10,12 +10,10 @@ function TopBar() {
             <Navbar bg="light" expand="lg" >
                 <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home"><img src={logo} /> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    </Nav>
-                    <Nav style={{ padding: "24px 40px 20px 40px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex" }} >
-                        <Nav.Link style={{ color: "#343538" }} href="#LogIn">Log In</Nav.Link>
-                        <Button variant="success" style={{ fontSize: "18px" }}>Sign Up</Button>{' '}
+                <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: "end" }}>
+                    <Nav style={{ padding: "24px 40px 20px 40px", fontSize: "18px", lineHeight: "26px", fontWeight: "bolder", display: "flex", justifyContent: "space-between" }} >
+                        <Nav.Link style={{ color: "#343538", marginRight: "20px" }} href="#LogIn">Log In</Nav.Link>
+                        <Button style={{ fontSize: "18px", backgroundColor: "rgb(10,173,10)", border: "rgb(10,173,10) solid 1px", borderRadius: "25px" }}>Sign Up</Button>{' '}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
